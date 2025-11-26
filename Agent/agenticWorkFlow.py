b361e88116ef44e5bb585ebfa55fe82d678b1c30
Kiran Prasad J P <kiranprasadjp@Kirans-MacBook-Pro-9.local>
2025-11-26 02:12:18 -0500

    app

--- Agent/agenticWorkFlow.py
+++ Agent/agenticWorkFlow.py
@@ -12,16 +12,48 @@ from tools.currencyConversion import CurrencyConverterTool
 
 class graphBuilder():
 
-    def __init__(self):
-        pass
+    def __init__(self,model_provider: str = "groq"):
+        self.model_loader = modelLoad(model_provider=model_provider)
+        self.llm = self.model_loader.load_llm()
+        
+        self.tools = []
+        
+        self.weather_tools = WeatherInfoTool()
+        self.place_search_tools = PlaceSearchTool()
+        self.calculator_tools = CalculatorTool()
+        self.currency_converter_tools = CurrencyConverterTool()
+        
+        self.tools.extend([* self.weather_tools.weather_tool_list, 
+                           * self.place_search_tools.place_search_tool_list,
+                           * self.calculator_tools.calculator_tool_list,
+                           * self.currency_converter_tools.currency_converter_tool_list])
+        
+        self.llm_with_tools = self.llm.bind_tools(tools=self.tools)
+        
+        self.graph = None
+        
+        self.system_prompt = SYSTEM_PROMPT
 
 
     def agentFunction(self):
-        pass
+        """Main agent function"""
+        user_question = state["messages"]
+        input_question = [self.system_prompt] + user_question
+        response = self.llm_with_tools.invoke(input_question)
+        return {"messages": [response]}
 
     def buildGraph(self):
-        pass
+        graphBuild=StateGraph(MessagesState)
+        graphBuild.add_node("agent", self.agent_function)
+        graphBuild.add_node("tools", ToolNode(tools=self.tools))
+        graphBuild.add_edge(START,"agent")
+        graphBuild.add_conditional_edges("agent",tools_condition)
+        graphBuild.add_edge("tools","agent")
+        graphBuild.add_edge("agent",END)
+        self.graph = graphBuild.compile()
+        return self.graph
+        
 
 
-    def __call__(self, *args, **kwds):
-        pass
+    def __call__(self):
+        return self.buildGraph()
